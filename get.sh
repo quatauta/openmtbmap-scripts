39b8dddd0410d42364b548fb17dc70a8cbe484ec
Daniel Schömer <daniel.schoemer@gmx.net>
2012-02-19 11:26:50 +0100

    Download with wget, it's not that fast (single connection) but the output is prettyier.

--- get.sh
+++ get.sh
@@ -5,8 +5,7 @@ URL_FILES="${@:-urls.txt}"
 pushd "$(dirname "${0}")"
 
 for URL_FILE in ${URL_FILES} ; do
-    URL_FILE="$(realpath "${URL_FILE}")"
-    aria2c --continue=false -j3 -x3 -R -i "${URL_FILE}"
+    wget -c -N -i "${URL_FILE}"
 done
 
 mmv -v '*.1' '#1'
