bbb3a3de0b251a4876838f2b0a653abf8397f640
Daniel Schömer <daniel.schoemer@gmx.net>
2012-02-19 11:27:50 +0100

    Don't show pushd/popd output.

--- get.sh
+++ get.sh
@@ -2,11 +2,11 @@
 
 URL_FILES="${@:-urls.txt}"
 
-pushd "$(dirname "${0}")"
+pushd "$(dirname "${0}")" >/dev/null
 
 for URL_FILE in ${URL_FILES} ; do
     wget -c -N -i "${URL_FILE}"
 done
 
 ls *.1 >/dev/null 2>&1 && mmv -v '*.1' '#1'
-popd
+popd >/dev/null
