e2ea52ca1fe17dc92e5b73f5bb29cb9bf1ad2ded
Daniel Schömer <daniel.schoemer@gmx.net>
2012-03-11 08:11:27 +0100

    get.sh unpacks mkgmap.

--- get.sh
+++ get.sh
@@ -9,4 +9,21 @@ for URL_FILE in ${URL_FILES} ; do
 done
 
 ls *.1 >/dev/null 2>&1 && mmv -v '*.1' '#1'
+
+# Unpack latest mkgmap.jar
+if [ mkgmap-latest.tar.gz -nt mkgmap.jar ] ; then
+    tar atf mkgmap-latest.tar.gz |
+    fgrep mkgmap.jar |
+    while read MKGMAP ; do
+        REV="${MKGMAP#mkgmap-r}"
+        REV="${REV%/*}"
+        NEW="mkgmap-${REV}.jar"
+        tar vaxf mkgmap-latest.tar.gz "${MKGMAP}" &&
+            mv  "${MKGMAP}" "${NEW}" &&
+            rm -f "mkgmap.jar" &&
+            ln -sv "${NEW}" "mkgmap.jar" &&
+            rm -rf mkgmap-r*/
+    done
+fi
+
 popd >/dev/null
