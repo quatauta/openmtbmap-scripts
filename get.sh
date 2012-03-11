0c1934b23639ae299ac23cb9c0999be6402bd1ed
Daniel Schömer <daniel.schoemer@gmx.net>
2012-03-11 08:12:43 +0100

    Don't continue downloads with wget. If sizese don't match, the upstream file has likely been updated.

--- get.sh
+++ get.sh
@@ -5,7 +5,7 @@ URL_FILES="${@:-urls.txt}"
 pushd "$(dirname "${0}")" >/dev/null
 
 for URL_FILE in ${URL_FILES} ; do
-    wget -c -N -i "${URL_FILE}"
+    wget -N -i "${URL_FILE}"
 done
 
 ls *.1 >/dev/null 2>&1 && mmv -v '*.1' '#1'
